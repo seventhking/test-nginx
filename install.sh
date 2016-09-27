22c50755fc4fccdca601e95583d077eb48e13915
seventhking <dqwang_coco@sina.com>
2016-09-27 10:01:23 +0800

    init

--- install.sh
+++ install.sh
@@ -3,10 +3,10 @@
 pwd=`pwd`
 if [ ! -d "nginx" ]; then
     cd ${pwd}/dependences
-    tar -xvzf nginx-1.8.1.tar.gz
+    tar -xvzf nginx-1.10.1.tar.gz
 fi
 
-cd ${pwd}/dependences/nginx-1.8.1
+cd ${pwd}/dependences/nginx-1.10.1
 ./configure --prefix=${pwd}/nginx --with-http_gunzip_module
 
 processor_num=`cat /proc/cpuinfo |grep processor |wc -l`
